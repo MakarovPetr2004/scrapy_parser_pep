66bcdc3f9125af0cbe25107bb21f0a430494c700
Макаров Пётр <mr.ru.20042004@gmail.com>
2023-12-24 23:24:26 +0300

    v 1.0.0

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,35 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import csv
+import datetime as dt
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+from pep_parse.constants import BASE_DIR
 
 
 class PepParsePipeline:
+
+    def open_spider(self, spider):
+        self.status_counter = {}
+
     def process_item(self, item, spider):
+
+        if item['status'] in self.status_counter:
+            self.status_counter[item['status']] += 1
+        else:
+            self.status_counter[item['status']] = 1
         return item
+
+    def close_spider(self, spider):
+        results_dir = BASE_DIR / 'results'
+        results_dir.mkdir(exist_ok=True)
+
+        now_time = dt.datetime.now()
+        filename = f'status_summary_{now_time}.csv'
+        file_path = results_dir / filename
+
+        with open(file_path, mode='w', encoding='utf-8') as f:
+            writer = csv.writer(f, dialect='unix', quoting=csv.QUOTE_NONE)
+
+            writer.writerow(('Статус', 'Количество'))
+
+            writer.writerows(list(self.status_counter.items()))
+
+            writer.writerow(('Total', sum(self.status_counter.values())))
